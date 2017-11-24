0a466b63dda3b12bec570c9da17f32854b258107
William Ashton <William@AshtonFam.org>
2017-11-24 11:01:17 -0500

    Initial commit

---
+++ lib/nbt.ex
@@ -0,0 +1,18 @@
+defmodule NBT do
+  @moduledoc """
+  Documentation for NBT.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NBT.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nbt_test.exs
@@ -0,0 +1,8 @@
+defmodule NBTTest do
+  use ExUnit.Case
+  doctest NBT
+
+  test "greets the world" do
+    assert NBT.hello() == :world
+  end
+end
